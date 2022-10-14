6702c4b21aadb72ed757d0226cd4bbe041e8c8c3
lilianabs <lilianabsmath@gmail.com>
2022-10-13 23:07:56 -0500

    Add baseline model

--- ml_pipeline/train_model.py
+++ ml_pipeline/train_model.py
@@ -10,12 +10,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 
+logging.basicConfig(
+    filename='../logs/churn_library.log',
+    level=logging.INFO,
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s')
+
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("fraud-detection")
 
-df = pd.read_csv("../data/raw/fraudTrain.csv")
-df = df.drop('Unnamed: 0', axis=1)
-
+try:
+    logging.info("Loading data")
+    df = pd.read_csv("../data/raw/fraudTrain.csv")
+    df = df.drop('Unnamed: 0', axis=1)
+except FileNotFoundError:
+    logging.error("CSV not found")
+    df = pd.DataFrame()
+    
 # Feature engineering
 df["trans_date_trans_time"] = pd.to_datetime(df["trans_date_trans_time"])
 df["hour"] = df.trans_date_trans_time.dt.hour
@@ -35,10 +46,32 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y,
                                                       random_state=1,
                                                       train_size=0.8)
 
-model = RandomForestClassifier(random_state=1)
-model.fit(X_train, y_train)
+with mlflow.start_run():
+    
+    mlflow.set_tag("experiment_type", "inital experiments")
+    
+    params = {
+        "n_estimators": 10,
+        "max_depth": 5
+    }
+    
+    mlflow.log_params(params)
+    
+    model = RandomForestClassifier(
+        **params,
+        random_state=1
+        )
+    
+    model.fit(X_train, y_train)
 
-predictions = model.predict(X_valid)
+    predictions = model.predict(X_valid)
 
-print(accuracy_score(y_valid, predictions, normalize=True))
-print(f1_score(y_valid, predictions))
+    acc = accuracy_score(y_valid, predictions, normalize=True)
+    f1_sc = f1_score(y_valid, predictions)
+    
+    metrics = {
+        "accuracy": acc,
+        "f1_score": f1_sc
+    }
+    
+    mlflow.log_metrics(metrics)
